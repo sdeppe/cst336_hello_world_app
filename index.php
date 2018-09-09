f81686fa4209b1fa8c38c7f82261c3fa28ae1716
Shawn Deppe <sdeppe@csumb.edu>
2018-09-09 19:46:35 +0000

    added css precedence testing

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hello World
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        <a href="/classwork/index.php">Main Class Link</a>
+
+    </body>
+</html>
