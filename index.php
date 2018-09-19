56efb3fac2f1c3afc1c14f968d9c379e03e93b8d
Shawn Deppe <sdeppe@csumb.edu>
2018-09-19 18:01:43 +0000

    main php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     </head>
     <body>
         <a href="/labs/1/index.html">Lab 1</a>
+        <br />
         <a href="/homework/1/index.html">Homework 1</a>
+        <br />
         <a href="/labs/2/index.php">Lab 2</a>
+        <br />
     </body>
 </html>
