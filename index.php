458c7b9f59f28acb317a87bafee47d462ea1d071
Shawn Deppe <sdeppe@csumb.edu>
2018-11-28 18:58:25 +0000

    updated index.php

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
         <title> </title>
     </head>
     <body>
-        <a href="/labs/1/index.html">Lab 1</a>
-        <br />
+        
         <a href="/homework/1/index.html">Homework 1</a>
         <br />
-        <a href="/labs/2/index.php">Lab 2</a>
-        <br />
         <a href="/homework/2/index.php">Homework 2</a>
+        <br />
+        <a href="/homework/4/index.php">Homework 4</a>
+        <br />
+        
+        <a href="/labs/1/index.html">Lab 1</a>
+        <br />
+        <a href="/labs/2/index.php">Lab 2</a>
         <br />
         <a href="/labs/4/index.php">Lab 4</a>
         <br />
@@ -21,6 +25,8 @@
         <a href="/labs/7/index.php">Lab 7</a>
         <br />
         <a href="/labs/8/index.html">Lab 8</a>
+        <br />
+        <a href="/labs/9/index.html">Lab 9</a>
         <br />
     </body>
 </html>
